1c0d1448e0d59eb3300d4aebb5886fe82b754b18
SunburtReynolds <jon.reynolds456@utexas.edu>
2016-02-26 18:06:03 -0600

    seemingly correct version of GS working; compared with test results on piazza

--- Lab1/Program1.java
+++ Lab1/Program1.java
@@ -3,6 +3,7 @@
 * EID: <your EID>
 */
 
+import java.util.Arrays;
 import java.util.ArrayList;
 import java.util.Dictionary;
 import java.util.Hashtable;
@@ -95,7 +96,148 @@ public class Program1 extends AbstractProgram1 {
     * @return A stable Matching.
     */
     public Matching stableMatchingGaleShapley(Matching given_matching) {
-        /* TODO implement this function */
-        return null; /* TODO remove this line */
+        int numTenant = given_matching.getTenantCount();
+        int numApartment = numTenant; /* interchangeable values */
+        int numLandlord = given_matching.getLandlordCount();
+
+        /* given data structures */
+        ArrayList<ArrayList<Integer>> tenantPref = given_matching.getTenantPref();
+        ArrayList<ArrayList<Integer>> landlordPref = given_matching.getLandlordPref();
+        ArrayList<ArrayList<Integer>> landlordOwn = given_matching.getLandlordOwners();
+
+        /* helper data structures */
+        Dictionary<Integer, Integer> apartmentToLandlord = new Hashtable<Integer, Integer>();
+        ArrayList<Tenant> tenants = new ArrayList<Tenant>();
+        ArrayList<Apartment> apartments = new ArrayList<Apartment>();
+        LinkedList<Tenant> freeTenants = new LinkedList<Tenant>();
+        Integer[] matching = new Integer[numTenant];
+
+        /* NOTE: an apartment has the same preference list as the landlord who owns it */
+
+        /* populate apartmentToLandlord; maps apartments to their landlords */
+        for (Integer landlordId = 0; landlordId < numLandlord; landlordId++) {
+            ArrayList<Integer> owns = landlordOwn.get(landlordId);
+            for (Integer aptId : owns) {
+                apartmentToLandlord.put(aptId, landlordId);
+            }
+        }
+
+        /* populate apartments; array indexed by id to each apartment's pref list */
+        for (Integer apartmentId = 0; apartmentId < numApartment; apartmentId++) {
+            Integer landlordId = apartmentToLandlord.get(apartmentId);
+            Apartment a = new Apartment(apartmentId, landlordPref.get(landlordId));
+            apartments.add(a);
+        }
+
+        /* populate tenants; array indexed by id to each tenant's pref list */
+        for (Integer tenantId = 0; tenantId < numTenant; tenantId++) {
+            Tenant t = new Tenant(tenantId, tenantPref.get(tenantId), apartments);
+            tenants.add(t);
+        }
+
+        /* populate the freeTenants with every tenant to begin with since none of them have been matched */
+        for (Integer tenantId = 0; tenantId < numTenant; tenantId++) {
+            freeTenants.add(tenants.get(tenantId));
+        }
+
+        /* initialize matching with -1 at every index; index of matching is the apartment, and the value at that index is the matched tenant */
+        for (Integer i = 0; i < matching.length; i++) {
+            matching[i] = -1;
+        }
+
+        /* algorithm based on Gale-Shapely; tenants propose to apartments (which each have the same preference list as their landlord owner) */
+
+        /* while there are free tenants */
+        while (freeTenants.size() > 0) {
+            /* choose some free tenant */
+            Tenant lonelyTenant = freeTenants.removeFirst();
+            /* choose the apartment highest on tenant's pref list that he has not proposed to yet */
+            Apartment apartment = lonelyTenant.nextApt();
+            /* if the apartment is not engaged, then lonelyTenant and apartment become engaged  */
+            if (matching[apartment.id] < 0) {
+                matching[apartment.id] = lonelyTenant.id;
+            } else {
+                /* otherwise, see which tenant the apartment prefers */
+                Integer currentTenantId = matching[apartment.id];
+                Tenant currentTenant = tenants.get(currentTenantId);
+                if (currentTenantId != currentTenant.id) { System.out.println("FUUUUUCK"); }
+                /* if the apartment strictly prefers lonelyTenant, engage lonelyTenant and apartment in matching, and add apartment's old tenant to the free list */
+                if (apartment.striclyPrefersFirstOverSecond(lonelyTenant, currentTenant)) {
+                    matching[apartment.id] = lonelyTenant.id;
+                    freeTenants.add(currentTenant);
+                } else {
+                    /* otherwise, add lonelyTenant back to the free list */
+                    freeTenants.add(lonelyTenant);
+                }
+            }
+        }
+
+        /* convert my matching to the necessary format; my indeces and values are inverse of the required format */
+        Integer[] matchingInverse = new Integer[numTenant];
+        for (Integer apartmentId = 0; apartmentId < numApartment; apartmentId++) {
+            Integer tenantId = matching[apartmentId];
+            matchingInverse[tenantId] = apartmentId;
+        }
+        ArrayList<Integer> matchingArrList = new ArrayList<Integer>(Arrays.asList(matchingInverse));
+        given_matching.setTenantMatching(matchingArrList);
+        return given_matching;
+    }
+
+    public class Tenant {
+        Integer id;
+        private ArrayList<Integer> rawPrefList; // the provided preference list for this tenant
+        /*
+         * prefList contains all of the apartments in order of preference for this tenant;
+         * at each index, there is a linked list of apartments (all of which have the same ranking for this tenant);
+         * if there is a unique ranking for every apartment for this tenant, then each linked list will
+         * have a length of one
+         */
+        // private LinkedList<Apartment>[] prefList;
+        private ArrayList<LinkedList<Apartment>> prefList;
+        private Integer next = 0;
+
+        public Tenant(Integer id, ArrayList<Integer> rawPrefList, ArrayList<Apartment> apartments) {
+            this.id = id;
+            this.rawPrefList = rawPrefList;
+            this.prefList = new ArrayList<LinkedList<Apartment>>(apartments.size());
+
+            /* initialize linked lists for each ranking */
+            for (Integer rank = 0; rank < apartments.size(); rank++) {
+                this.prefList.add(new LinkedList<Apartment>());
+            }
+
+            for (Apartment apt : apartments) {
+                Integer scaledRanking = this.rawPrefList.get(apt.id) - 1; // ranking should be 0-indexed
+                this.prefList.get(scaledRanking).add(apt);
+            }
+        }
+
+        /* this function returns the next highest ranking apartment for this tenant */
+        public Apartment nextApt() {
+            /* while there is no apartment at the current ranking, go to the next ranking */
+            while (prefList.get(next).size() == 0) {
+                next++;
+            }
+            /* grab the first apartment at the highest ranking */
+            Apartment a = prefList.get(next).removeFirst();
+            return a;
+        }
+    }
+
+    public class Apartment {
+        Integer id;
+        private ArrayList<Integer> prefList; // the ranking as given by given_matching for this apartment (i.e. landlord's pref)
+
+        public Apartment(Integer id, ArrayList<Integer> prefList) {
+            this.id = id;
+            this.prefList = prefList;
+        }
+
+        public Boolean striclyPrefersFirstOverSecond(Tenant t1, Tenant t2) {
+            Integer pref1 = prefList.get(t1.id);
+            Integer pref2 = prefList.get(t2.id);
+
+            return pref1 < pref2;
+        }
     }
 }
